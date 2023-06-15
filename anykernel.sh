fdf26b8a8c9c2348c8cc2ae5d34b611b9e8db5c2
Kartik728 <titanupdates728@gmail.com>
2023-06-15 21:52:49 +0530

    RMX1851: init bringup for B.E.A.M
    Signed-off-by: Kartik728 <titanupdates728@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -4,18 +4,15 @@
 ### AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=ExampleKernel by osm0sis @ xda-developers
+kernel.string=B.E.A.M Kernel for Realme 3 Pro [RMX1851]
 do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=maguro
-device.name2=toro
-device.name3=toroplus
-device.name4=tuna
-device.name5=
-supported.versions=
+device.name1=RMX1851
+device.name2=RMX1851CN
+supported.versions=10.0-13.0
 supported.patchlevels=
 '; } # end properties
 
@@ -28,7 +25,7 @@ set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
 
 
 ## boot shell variables
-block=/dev/block/platform/omap/omap_hsmmc.0/by-name/boot;
+block=/dev/block/by-name/boot;
 is_slot_device=0;
 ramdisk_compression=auto;
 patch_vbmeta_flag=auto;
